cca8a8ba8b2b672c6510ce78d1ae17a6a3abe71b
Shwu <shubham.kushwaha@nyunai.com>
2024-07-09 20:16:40 +0000

    engine: tensorrtllm 🚀

--- __init__.py
+++ __init__.py
@@ -12,11 +12,26 @@ def _import_AutoAWQ() -> Algorithm:
     return AutoAWQ
 
 
+# ===================================
+#               engine
+# ===================================
+
+
+def _import_TensorRTLLM():
+    from .engines.tensorrt_llm import TensorRTLLM
+
+    return TensorRTLLM
+
+
 def __getattr__(name: str) -> Algorithm:
 
     # quantization
     if name == "AutoAWQ":
         return _import_AutoAWQ()
+
+    # engine
+    elif name == "TensorRTLLM":
+        return _import_TensorRTLLM()
 
     else:
         raise AttributeError(f"Unsupported algorithm: {name}")
