c08d909bd2fb32cbb354119e04a6607e2241f1d2
Ubuntu <ishan@ishanT4-disk-large.3i215ca3foluxhfbt1psrpy3qh.bx.internal.cloudapp.net>
2024-12-21 12:10:05 +0000

    hotfix: more robust  get_rank function

--- quantization/aqlm/utils/distributed.py
+++ quantization/aqlm/utils/distributed.py
@@ -4,7 +4,10 @@ import torch.distributed
 def get_rank():
     if torch.cuda.is_available() and torch.distributed.is_available():
         if not torch.distributed.is_initialized():
-            torch.distributed.init_process_group()
+            try:
+                torch.distributed.init_process_group(backend='nccl')
+            except ValueError:
+                return 0
         rank = torch.distributed.get_rank()
     else:
         rank = 0
